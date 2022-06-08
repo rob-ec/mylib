29e3fcf9a055270fc346d7be422214ac0305874c
Robson Gomes <robson.mesquita56@gmail.com>
2022-06-08 16:51:30 -0300

    feat: Add página 404

---
+++ source/frontend/src/pages/NoMach/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css"
+
+export default function NoMach() {
+    return (
+        <section className="nomach">
+            <h1 className="nomach-404">
+                <span className="nomach-4">4</span>
+                <span className="nomach-0">0</span>
+                <span className="nomach-4">4</span>
+            </h1>
+        </section>
+    )
+}
--- source/frontend/src/routes.js
+++ source/frontend/src/routes.js
@@ -1,15 +1,17 @@
 import React from "react";
-
 import { Routes, Route } from "react-router-dom";
-import About from "./pages/About";
+import Livro from "./pages/Livro";
 import Livros from "./pages/Livros";
+import NoMach from "./pages/NoMach";
 
 export default function MainRoutes() {
     return(
         <Routes>
             <Route path="/" element={<Livros />}/>
             <Route path="/livros" element={<Livros />}/>
-            <Route path="/about" element={<About />}/>
+            <Route path="/livro/:id" element={<Livro />}/>
+            <Route path="/404" element={<NoMach />}/>
+            <Route path="*" element={<NoMach />}/>
         </Routes>
     );
 }
